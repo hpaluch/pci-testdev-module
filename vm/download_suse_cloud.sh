e4df1176ece0efaa0e1d39670ddfa0a55efafcfe
Henryk Paluch <hpaluch@seznam.cz>
2022-07-30 10:09:41 +0200

    Finished suse image download script

--- vm/download_suse_cloud.sh
+++ vm/download_suse_cloud.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 set -x
 cd $(dirname $0)
-curl -fLO https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.3/images/openSUSE-Leap-15.3.x86_64-NoCloud.qcow2.sha256
-curl -fLO https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.3/images/openSUSE-Leap-15.3.x86_64-NoCloud.qcow2
+URL=https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.3/images/openSUSE-Leap-15.3.x86_64-NoCloud.qcow2
+FNAME="${URL//*\//}"
+curl -fLO $URL.sha256
+curl -fLO $URL
+sha256sum -c $FNAME
 exit 0
